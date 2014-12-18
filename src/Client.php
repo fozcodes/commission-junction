56d1df3124d195ad781b9917fc94f9fff3e74f96
Ian Fosbery <ian@nmrkt.com>
2014-12-18 17:56:17 +0000

    added some deps; trying to change to guzzle 5

--- src/Client.php
+++ src/Client.php
@@ -8,9 +8,24 @@
 
 namespace Nmrkt\CommissionJunction;
 
+use Guzzle\Service\Client as GuzzleClient;
+use Guzzle\Service\Description\ServiceDescription;
 
-class Client {
+class Client extends GuzzleClient
+{
 
+	public function __construct($access_token)
+	{
 
+		parent::__construct();
+
+		// Set the service description
+		$this->setDescription(ServiceDescription::factory(__DIR__.'/Resources/bitly.json'));
+
+		$this->addSubscriber(new TokenAuthPlugin($access_token));
+		$this->addSubscriber(new ArrayAggregatorSubscriber());
+		$this->addSubscriber(new ResponseStandardizationSubscriber());
+
+	}
 
 }
